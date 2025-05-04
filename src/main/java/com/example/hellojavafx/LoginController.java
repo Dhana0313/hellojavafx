300f00e293f2e49d1f75b402b0d03a056c9ad9b6
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-05-04 11:42:51 +0530

    Create fxml file

---
+++ src/main/java/com/example/hellojavafx/LoginController.java
@@ -0,0 +1,17 @@
+package com.example.hellojavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button cancelButton;
+
+    public void cancelButtonOnAction(ActionEvent event){
+        Stage stage=(Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+}
