3e2b0754b872f6b0fadf2a23755387fe26dab902
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-05-04 12:36:15 +0530

    Add login button action

--- src/main/java/com/example/hellojavafx/LoginController.java
+++ src/main/java/com/example/hellojavafx/LoginController.java
@@ -1,14 +1,40 @@
 package com.example.hellojavafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
-public class LoginController {
+import javax.swing.*;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
 
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("Images/images.jpeg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+    }
+
+
+    public void loginButtonOnAction(ActionEvent event){
+        loginMessageLabel.setText("You try to login");
+
+    }
 
     public void cancelButtonOnAction(ActionEvent event){
         Stage stage=(Stage) cancelButton.getScene().getWindow();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.hellojavafx {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
+    requires java.desktop;
     //requires mysql.connector.java;
 
     opens com.example.hellojavafx;
